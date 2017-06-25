408339f3ba6379e1ace34aec973ef78e28aad774
nasoundead <nasoundead@163.com>
2017-06-25 15:25:12 +0800

    first commit

---
+++ telnet.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""hello world
+"""
+
+def main():
+    "main function"
+    print "hello world"
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,13 @@
+import re
+import os
+
+a_list = [1, 2, 3]
+
+for i in a_list:
+    print(i)
+
+def print_hello(name):
+    print('hello'+name)
+
+
+print_hello("Wang Haibo")
